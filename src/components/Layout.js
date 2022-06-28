49465920d66254f2c9dba496f9f61015c4ffce17
mrjohnming <john.ngai@hotmail.com>
2022-06-28 15:06:55 -0400

    Render <Header /> & <Footer />

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,14 @@
+// Components
+import Header from './Header';
+import Footer from './Footer';
+// Stylesheet
 import './Layout.scss';
 
 export default function Layout() {
   return (
-    <div className='container--layout'></div>
+    <div className='container--layout'>
+      <Header />
+      <Footer />
+    </div>
   );
 }
