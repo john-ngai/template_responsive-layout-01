9ace41b1c6bb2b990ab5e4395962f842a3f5ab94
mrjohnming <john.ngai@hotmail.com>
2022-06-28 16:36:25 -0400

    Add @fortawesome packages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 // Components
 import App from './App';
+
+library.add(fab);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
