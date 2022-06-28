59f0960e0870190cc1533878a1a95ec3b9fe9e89
mrjohnming <john.ngai@hotmail.com>
2022-06-28 16:42:08 -0400

    Create component - Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import './Navigation.scss';
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
