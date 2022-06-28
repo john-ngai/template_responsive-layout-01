b13f75dfeafc1714b7254343c350a68dbb813644
mrjohnming <john.ngai@hotmail.com>
2022-06-28 14:44:25 -0400

    Render ReactDOM

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Packages
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+// Components
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
