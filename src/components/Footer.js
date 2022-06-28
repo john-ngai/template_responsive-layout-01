90da0f7c63023dac9a57abb1cf30aa8a055b7b8e
mrjohnming <john.ngai@hotmail.com>
2022-06-28 16:37:26 -0400

    Create & style <Footer /> content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,35 @@
+// Packages
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// Stylesheet
 import './Footer.scss';
 
 export default function Footer() {
   return (
-    <div>Component - Footer</div>
+    <div className='container--footer'>
+      <div className='social-media'>
+        <span className='icon'>
+          <FontAwesomeIcon
+            icon={['fab', 'linkedin']}
+          />
+        </span>
+        <span className='icon'>
+          <FontAwesomeIcon
+            icon={['fab', 'github']}
+          />
+        </span>
+        <span className='icon'>
+          <FontAwesomeIcon
+            icon={['fab', 'instagram']}
+          />
+        </span>
+      </div>
+
+      <p className='copyright'>
+        &copy; 2022 Company. Designed by <a
+          href='https://johnngai.com/'
+          target='_blank'
+        >John Ngai</a>.
+      </p>
+    </div>
   );
 }
