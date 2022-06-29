a8f81a8098e2f8da12efe89902e4e7042558215d
mrjohnming <john.ngai@hotmail.com>
2022-06-29 16:56:35 -0400

    Create sliding menu (mobile view only)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,42 @@
+// Packages
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// Stylesheet
 import './Navigation.scss';
 
 export default function Navigation() {
   return (
     <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <input className='menu-btn' type='checkbox' id='menu-btn' />
+      <label className='menu-icon' for='menu-btn'>
+        <span className='navicon' />
+      </label>
+
+      <ul className='menu'>
+        <div className='menu--nav'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </div>
+
+        <div className='menu--social'>
+          <span className='icon'>
+            <FontAwesomeIcon
+              icon={['fab', 'linkedin']}
+            />
+          </span>
+          <span className='icon'>
+            <FontAwesomeIcon
+              icon={['fab', 'github']}
+            />
+          </span>
+          <span className='icon'>
+            <FontAwesomeIcon
+              icon={['fab', 'instagram']}
+            />
+          </span>
+        </div>
       </ul>
-    </nav>
+      <div className='shader' />
+    </nav >
   );
 }
