9716d697a79a72a1f75d3094d2435f0264a7863b
mrjohnming <john.ngai@hotmail.com>
2022-06-30 19:35:00 -0400

    Create <Route /> paths - index, about, & contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import { Route, Routes } from 'react-router-dom';
 // Components
 import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 // Stylesheet
 import './style.scss';
 
@@ -10,7 +13,11 @@ export default function App() {
     <Routes>
       <Route path='/'
         element={<Layout />}
-      />
+      >
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+      </Route>
     </Routes>
   );
 }
